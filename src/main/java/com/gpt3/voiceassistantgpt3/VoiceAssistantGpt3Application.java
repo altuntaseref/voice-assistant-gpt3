5b3b1fbadb1c16cd695b611d401e192e31798a3d
altuntaseref <seref.altuntas.ea@gmail.com>
2023-05-01 11:17:04 +0300

    Initial commit

---
+++ src/main/java/com/gpt3/voiceassistantgpt3/VoiceAssistantGpt3Application.java
@@ -0,0 +1,13 @@
+package com.gpt3.voiceassistantgpt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceAssistantGpt3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceAssistantGpt3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gpt3/voiceassistantgpt3/VoiceAssistantGpt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpt3.voiceassistantgpt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceAssistantGpt3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
